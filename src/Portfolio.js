9b8a982c118a134bf6442b90c80c0f4d44d9bcb3
BarbaraSheaKenny <b.kenny58@gmail.com>
2021-06-18 14:15:06 -0400

    file structure

---
+++ src/Portfolio.js
@@ -0,0 +1,49 @@
+let portfolioData = {
+    "image":
+    "name":
+    "role":
+    "roleDescription":
+    "socialMedia": [
+        {
+            "name": "linkedin",
+            "url": "https://www.linkedin/BarbaraShea"
+        },
+        {
+            "name":
+            "url":
+        }, 
+        {
+            "name":
+            "url":
+        },
+    ]
+    "aboutMe":
+    "education": [
+        {
+            "University":
+            "Degree":
+            "Year":
+            "Achievements":
+        },
+         {
+            "University":
+            "Degree":
+            "Year":
+            "Achievements":
+        }, 
+        {
+            "University":
+            "Degree":
+            "Year":
+            "Achievements":
+        },
+    ]
+    "skills":
+    "portfolio":
+    
+
+
+
+
+
+}
